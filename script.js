26fedff1ce2af3e01d2110cd665c114c083a990f
Ali Maaref <alimaaref1383@gmail.com>
2026-09-12 20:14:39 +0330

    complete music functionality

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const overlay = document.querySelector('.overlay')
 const sidebar = document.querySelector('.sidebar')
 const closeBtn = document.querySelector('.close-btn')
 const navParts = document.querySelectorAll('.sidebar nav a')
+/*
+const playBtns = document.querySelectorAll('play-btn-track')
+const pauseBtns = document.querySelectorAll('pause-btn-track')
+ */
+const tracks = document.querySelectorAll('.track')
 
 //sidebar functionality
-
 menuBtn.addEventListener('click',()=>{
     sidebar.classList.add('active')
     overlay.classList.add('active')
@@ -22,3 +26,39 @@ navParts.forEach((navpart)=>{
         overlay.classList.remove('active')
     })
 })
+
+//songs funcationality
+
+
+tracks.forEach((track) =>{
+    const playBtn = track.querySelector('.play-btn-track')
+    const pauseBtn = track.querySelector('.pause-btn-track')
+    const audio = track.querySelector('audio')
+    playBtn.addEventListener('click',()=>{
+        stopOther()
+        audio.play()
+        playBtn.classList.add('played')
+        pauseBtn.classList.add('played')
+    })
+    pauseBtn.addEventListener('click',()=>{
+        audio.pause()
+        playBtn.classList.remove('played')
+        pauseBtn.classList.remove('played')
+    })
+})
+
+function stopOther(){
+    const audios  = document.querySelectorAll('audio')
+    const playBtns = document.querySelectorAll('.play-btn-track')
+    const pauseBtns = document.querySelectorAll('.pause-btn-track')
+    audios.forEach(audio => {
+        audio.pause()
+        playBtns.forEach(playBtn =>{
+            playBtn.classList.remove('played') 
+        })
+        pauseBtns.forEach(pauseBtn =>{
+            pauseBtn.classList.remove('played')      
+        })
+    })
+}
+
